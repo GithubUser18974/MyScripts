c0d4877313a5d6f74478328a304bf3fbed35ef99
Mohamed Araby <mohamedaraby1296@gmail.com>
2022-04-21 02:47:53 +0200

    Add files via upload

---
+++ CheckCheating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCheating : MonoBehaviour
+{
+     string url = "https://raw.githubusercontent.com/mohamedaraby122/Validations/master/BookFair.json";
+    void Start()
+    {
+        StartCoroutine(StartUrl());
+    }
+    IEnumerator StartUrl()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        if (www.error == null)
+        {
+            string s = www.text;
+            if (s.Contains("false"))
+            { Application.Quit(); }
+            
+        }
+    }
+}
